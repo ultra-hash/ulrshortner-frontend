8509fea1e1b0b5fffef4d1d4d05262c93b34fdab
veerabhadra swamy uppu <78792323+ultra-hash@users.noreply.github.com>
2023-10-24 15:30:08 +0530

    feat: Implemented profile component
    - added profile compoent with basic structure
    - fetchs user's details from the backend

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,39 @@
+import { useState, useEffect } from 'react'
+import { api } from '../../config'
+
 export default function Profile() {
-    return (
+    const [first_name, setFirst_name] = useState('')
+    const [last_name, setLast_name] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone_number, setPhone_number] = useState('')
+    const [username, setUsername] = useState('')
+
+    const callGetUserDetailsApi = async () => {
+        const { BaseUrl, Port } = api
+        const url = BaseUrl + ':' + Port + '/user/user-details?username=johncena'
+
+        const response = await fetch(url)
+        const data = await response.json()
+
+        setFirst_name(data.first_name)
+        setLast_name(data.last_name)
+        setEmail(data.email_id)
+        setPhone_number(data.phone_number)
+        setUsername(data.username)
+
+    }
+
+    useEffect(() => { callGetUserDetailsApi() }, [])
+
+    return (<>
         <h1>Profile</h1>
+        <small>TODO:// default username: johncena</small>
+        <div style={{ border: "1px solid black", padding: "10px", margin: "10px" }}>
+            <p>Name: <b> {first_name} {last_name}</b></p>
+            <p>Email: <b>{email}</b></p>
+            <p>username: <b>{username}</b></p>
+            <p>phone_number: <b>{phone_number}</b></p>
+        </div>
+    </>
     )
 }
