3a63ed620ded728a54db4df7f22593d816f75088
veerabhadra swamy uppu <78792323+ultra-hash@users.noreply.github.com>
2023-10-27 13:49:42 +0530

    Refactor: Centralized API URLs in the configuration file.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,19 @@
-const BaseUrl = "http://localhost"
-const Port = 2023
+const PROTOCOL = "http://"
+const HOST = "localhost"
+const PORT = 2023
+const BASE_URL = PROTOCOL + HOST + ":" + PORT
 
 export const api = {
-    BaseUrl,
-    Port,
-    RedirectUrl: `${BaseUrl}:${Port}/url`,
+    BASE_URL,
+    routes: {
+        urls: {
+            CreateUrl: `${BASE_URL}/url/create`,
+            RedirectUrl: `${BASE_URL}/url/redirect`,
+        },
+        users: {
+            RegisterUser: `${BASE_URL}/user/register`,
+            LoginUser: `${BASE_URL}/user/login`,
+            GetUserDetails: `${BASE_URL}/user/user-details`
+        }
+    }
 }
